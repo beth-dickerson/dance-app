a398cc6fe9adb9e76386a24c3696e820cb4ce896
Beth Dickerson <bethdickerson17@gmail.com>
2017-09-16 22:00:36 -0400

    configured webpacker and connected static page for splash

---
+++ react/webpack.config.js
@@ -0,0 +1,36 @@
+var config = {
+  entry: {
+        path: __dirname + '/src/main.js',
+  },
+  output: {
+        path: __dirname + '/../app/assets/javascripts',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel'
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options :  {
+          presets  :  [ 'es2015', 'stage-2' ] // stage-2 if required
+        }
+      }
+    ]
+  },
+  devtool: 'eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  delete config.devtool;
+  var webpack = require('webpack');
+  config.plugins = [
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' })
+  ];
+}
+
+module.exports = config;
