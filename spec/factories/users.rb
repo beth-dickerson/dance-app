5de0975ac2416c2da614eb0e3ae5a1aef706be70
Beth Dickerson <bethdickerson17@gmail.com>
2017-09-19 12:01:26 -0400

    more user tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |n| "person#{n}@example.com" }
+    first_name 'Betty'
+    last_name 'Rubble'
+    password 'password'
+    password_confirmation 'password'
   end
 end
--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -14,7 +14,7 @@ feature 'user signs up', %Q{
   # * If I do not perform the above, I get an error message
   # * If I specify valid information, I register my account and am authenticated
 
-  scenario 'specify valid and required information' do
+  scenario 'user specifies valid and required information' do
     visit root_path
     click_link 'Sign Up'
     fill_in 'First Name', with: 'Jon'
@@ -28,7 +28,7 @@ feature 'user signs up', %Q{
     expect(page).to have_content("Sign Out")
   end
 
-  scenario 'required information is not supplied' do
+  scenario 'user specifies invalid information' do
     visit root_path
     click_link 'Sign Up'
     click_button 'Sign Up'
